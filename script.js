652b5f142a788de6302af05efab9810b31031b2f
Ivan <ivanesptic2@gmail.com>
2023-08-02 18:47:54 +0200

    added function to create results as DOM paragraphs

--- script.js
+++ script.js
@@ -1,60 +1,36 @@
-// Returns Rock,Paper or Scissors randomly to use in the playRound function 
-function getComputerChoice(){
-    let option = Math.floor(Math.random()* 3);
-
-    if (option === 0){
-        return "Rock";
-    }else if (option ===1) {
-        return "Paper";
-    } else {
-        return "Scissors";
-    }
-}
-//Simulates a round of Rock Paper Scissors with the value from getComputerChoice() and a value which the player inputs
-function playRound(playerSelection, computerSelection){
-    if(typeof playerSelection !== 'string'){
-        console.log("Numbers are not valid") 
-    }
-    else if(playerSelection.toLowerCase() ==="rock" && computerSelection === "Rock"){
-        console.log("Rock ties with Rock");
-    }
-    else if(playerSelection.toLowerCase() ==="rock" && computerSelection === "Paper"){
-        console.log("You lose! Rock loses to Paper");
-        return "Loss";
-    }
-    else if(playerSelection.toLowerCase() ==="rock" && computerSelection === "Scissors"){
-        console.log("You WIN! Rock smashes Scissors!");
-        return "Win";
-    }
-    else if(playerSelection.toLowerCase() ==="paper" && computerSelection === "Rock"){
-        console.log("You WIN! Paper beats Rock");
-        return "Win";
-    }
-    else if(playerSelection.toLowerCase() ==="paper" && computerSelection === "Paper"){
-        console.log("Paper ties with Paper");
-    }
-    else if(playerSelection.toLowerCase() ==="paper" && computerSelection === "Scissors"){
-        console.log("You lose! Paper loses to Scissors");
-        return "Loss";
-
-    }
-    else if(playerSelection.toLowerCase() ==="scissors" && computerSelection === "Rock"){
-        console.log("You lose! Scissors lose to Rock");
-        return "Loss";
-
-    }
-    else if(playerSelection.toLowerCase() ==="scissors" && computerSelection === "Paper"){
-        console.log("You WIN! Scissors cut Paper")
-        return "Win";
-    }
-    else if(playerSelection.toLowerCase() ==="scissors" && computerSelection === "Scissors"){
-        console.log("Scissors tie with Scissors")
-    }
-}
+const results = document.querySelector("#results");
+let pScore = 0;
+let cScore = 0;
+//Buttons
 const buttons = document.querySelectorAll("button");
 
-buttons.forEach((button) => {
+function showResult(result,pSelection,cSelection){
 
+    const createP = document.createElement('p');
+    switch(result){
+        case "Win":
+        pScore++;
+        createP.textContent = `You WIN!${pSelection} beats ${cSelection}\nPlayer Score = ${pScore}\nComputer Score = ${cScore}`;
+        createP.style.color = "green";
+        results.appendChild(createP);
+        break;
+
+        case "Loss":
+        cScore++;
+        createP.textContent = `You LOSE!${cSelection} beats ${pSelection}\nPlayer Score = ${pScore}\nComputer Score = ${cScore}`;
+        createP.style.color = "red";
+        results.appendChild(createP);
+        break;
+
+        case "Tie":
+        createP.textContent = `You TIED! ${pSelection} ties with ${cSelection}\nPlayer Score = ${pScore}\nComputer Score = ${cScore}`;
+        createP.style.color = "grey";
+        results.appendChild(createP);
+        break;
+    }
+}
+
+buttons.forEach((button) => {
     // and for each one we add a 'click' listener
     button.addEventListener('click', () => {
         if(button.id === "rock"){
@@ -67,22 +43,46 @@ buttons.forEach((button) => {
     });
   });
 
-/*
-function game(){
-    let PlayerWinCounter = 0;
-    let ComputerWinCounter = 0;
-    for(let i = 0; i<5; i++){
-        let playerSelection = prompt("Enter Rock Paper or Scissors");
-        const computerSelection = getComputerChoice();
-        let result =playRound(playerSelection,computerSelection);
-        if (result == "Win"){
-            PlayerWinCounter++;
-        }else if (result == "Loss"){
-            ComputerWinCounter++;
-        }
-        
+// Returns Rock,Paper or Scissors randomly to use in the playRound function 
+function getComputerChoice(){
+    let option = Math.floor(Math.random()* 3);
 
+    if (option === 0){
+        return "rock";
+    }else if (option ===1) {
+        return "paper";
+    } else {
+        return "scissors";
     }
-    console.log(`Player wins: ${PlayerWinCounter} \nComputer wins:${ComputerWinCounter}`)
 }
-*/
+
+//Simulates a round of Rock Paper Scissors with the value from getComputerChoice() and a value which the player inputs
+function playRound(playerSelection, computerSelection){
+    if(playerSelection === "rock" && computerSelection === "rock"){
+        return showResult("Tie",playerSelection,computerSelection);
+    }
+    else if(playerSelection === "rock" && computerSelection === "paper"){
+        return showResult("Loss",playerSelection,computerSelection);        
+    }
+    else if(playerSelection === "rock" && computerSelection === "scissors"){
+        return showResult("Win",playerSelection,computerSelection);        
+    }
+    else if(playerSelection === "paper" && computerSelection === "rock"){
+        return showResult("Win",playerSelection,computerSelection);        
+    }
+    else if(playerSelection === "paper" && computerSelection === "paper"){
+        return showResult("Tie",playerSelection,computerSelection);
+    }
+    else if(playerSelection === "paper" && computerSelection === "scissors"){
+        return showResult("Loss",playerSelection,computerSelection);        
+    }
+    else if(playerSelection === "scissors" && computerSelection === "rock"){
+        return showResult("Loss",playerSelection,computerSelection);        
+    }
+    else if(playerSelection === "scissors" && computerSelection === "paper"){
+        return showResult("Win",playerSelection,computerSelection);        
+    }
+    else if(playerSelection === "scissors" && computerSelection === "scissors"){
+        return showResult("Tie",playerSelection,computerSelection);
+    }
+}
